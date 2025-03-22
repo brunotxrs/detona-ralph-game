821a0b95aaedf8d8e4ea0b138a9cd6fcbb8110ff
brunotxrs <elielsonbrunno@gmail.com>
2025-03-22 12:42:55 -0400

    feat: import the components HomeScreen|GameScreen|NameProvider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from 'react';  
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import HomeScreen from './components/homescreen/HomeScreen';
+import GameScreen from './components/gamescreen/GameScreen'
+import { NameProvider } from './components/namecontext/NameContext';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <NameProvider>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/next-page" element={<GameScreen />} />
+      </Routes>
+    </BrowserRouter>
+    </NameProvider>
   );
 }
 
