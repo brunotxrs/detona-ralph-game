a3d66af1544b7fdf141dd537c3cf67ae46d2607a
brunotxrs <elielsonbrunno@gmail.com>
2025-03-22 12:36:44 -0400

    feat: import the NameProvider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { NameProvider } from './components/namecontext/NameContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <NameProvider>
     <App />
+    </NameProvider>
   </React.StrictMode>
 );
 
