24f44f2d60724ea48dbac764a01e6272b5537dbd
brunotxrs <elielsonbrunno@gmail.com>
2025-03-22 19:38:04 -0400

    feat: create the dashboard of component GameScreens

--- src/components/gamescreen/GameScreen.js
+++ src/components/gamescreen/GameScreen.js
@@ -1,15 +1,61 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { NameContext } from '../namecontext/NameContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import './GameScreen.css';
+import { faHeart } from '@fortawesome/free-solid-svg-icons/faHeart';
+import { faClock, faStar } from '@fortawesome/free-solid-svg-icons';
+
 
 
 function GameScreen() {
   const { playerName } = useContext(NameContext);
+  const [ timeRemaining, setTimeRemaining] = useState(60);
+
+  useEffect(() => {
+    if(timeRemaining > 0) {
+      const time = setInterval(() => {
+        setTimeRemaining(timeRemaining - 1);
+      }, 1000);
+
+      return () => clearInterval(time);
+    }
+  }, [timeRemaining]);
+
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes.toString().padStart('0')}:${seconds.toString().padStart(2, '0')}`;
+  }
+
 
   return (
     <div className='gamescreen'>
-      <h1 className='title'>Welcome, {playerName}!</h1>
+
+      <div className='game-painel'>
+
+        <div className='time'>
+        <span className='clock'><FontAwesomeIcon icon={faClock} /></span>
+        <span className='clock-n'>{formatTime(timeRemaining)}</span>
+        </div>
+
+        <div className='life'>
+          <span className='heart'><FontAwesomeIcon icon={faHeart} /></span>
+          <span>X <span className='number-heat'>3</span></span>
+          
+        </div>        
+      </div>
+
+      <div className='painel-player'>
+      <span>{playerName}</span>
+      <span className='score'><FontAwesomeIcon icon={faStar} /></span>
+      <span> X <span className='number-score'>0</span></span>
+
+      </div>
+
+
+
     </div>
   );
 }
